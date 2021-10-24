c875690788ce651bd45f494a280bd8c8a643497d
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-10-23 21:00:47 -0300

    final

--- rigged-dice.py
+++ rigged-dice.py
@@ -1,51 +1,148 @@
-from random import random
+import random
+
+def NormalDiceRoll():
+	face = random.randint(1, 6)
+	if (face == 1):
+		face = 111
+	if (face == 2):
+		face = 222
+	if (face == 3):
+		face = 333
+	if (face == 4):
+		face = 444
+	if (face == 5):
+		face = 555
+	if (face == 6):
+		face = 666
+	return face
+
+
+def toggleViciado(viciado):
+	if(viciado):
+		return False
+	else:
+		return True
+	
 
 def pointsInGameWithRoundsCount(roundAmount):
 	i=0
-	rollsAmount = [0, 0, 0, 0, 0, 0, 0]
-	totalPoints = 0
+	rollsAmountA = [0, 0, 0, 0, 0, 0, 0]
+	rollsAmountB = [0, 0, 0, 0, 0, 0, 0]
+	totalPointsA = 0
+	totalPointsB = 0
+	quemTaViciado = random.randint(1,2)
+	AViciado = False
+	BViciado = False
 
-	while(i<roundAmount):
-		roll = round(0.5+6*random())
+	if(quemTaViciado == 1):
+		AViciado = True
+		BViciado = False
+	if(quemTaViciado == 2):
+		AViciado = False
+		BViciado = True		
 
-		if roll == 1:
-			rollsAmount[roll] += 111
-		if roll == 2:
-			rollsAmount[roll] += 222
-		if roll == 3:
-			rollsAmount[roll] += 333
-		if roll == 4:
-			rollsAmount[roll] += 444
-		if roll == 5:
-			rollsAmount[roll] += 555
-		if roll == 6:
-			if i+1 == roundAmount:
-				rollsAmount[roll] += 666
-			else:
-				rollsAmount[roll] += 666*2
-				i += 1
-		i+=1
+	while(i<int(roundAmount)):
+		if(AViciado):
+			rollA = random.randint(1, 6)
+			rollB = NormalDiceRoll()
 
+			if(rollB == 111):
+				rollsAmountB[1] += rollB
+			if(rollB == 222):
+				rollsAmountB[2] += rollB
+			if(rollB == 333):
+				rollsAmountB[3] += rollB
+			if(rollB == 444):
+				rollsAmountB[4] += rollB
+			if(rollB == 555):
+				rollsAmountB[5] += rollB
+			if(rollB == 666):
+				rollsAmountB[6] += rollB
 
-	for r in rollsAmount:
-		totalPoints += r
+			if rollA == 1:
+				rollsAmountA[rollA] += 111
+			if rollA == 2:
+				rollsAmountA[rollA] += 222
+			if rollA == 3:
+				rollsAmountA[rollA] += 333
+			if rollA == 4:
+				rollsAmountA[rollA] += 444
+			if rollA == 5:
+				rollsAmountA[rollA] += 555
+			if rollA == 6:
+				if i+1 == roundAmount:
+					rollsAmountA[rollA] += 666
+				else:
+					rollsAmountA[rollA] += 666*2
+					rollB = NormalDiceRoll()
+					rollsAmountB[int(rollB/111)] += rollB
+					i += 1
 
-	return totalPoints
+			if(rollA != rollB/111):
+				if(AViciado):
+					AViciado = False
+					BViciado = True
+				else:
+					AViciado = True
+					BViciado = False
+			i+=1
+
+		else:
+			rollA = NormalDiceRoll()
+			rollB = random.randint(1, 6)
+
+			if(rollA == 111):
+				rollsAmountA[1] += rollA
+			if(rollA == 222):
+				rollsAmountA[2] += rollA
+			if(rollA == 333):
+				rollsAmountA[3] += rollA
+			if(rollA == 444):
+				rollsAmountA[4] += rollA
+			if(rollA == 555):
+				rollsAmountA[5] += rollA
+			if(rollA == 666):
+				rollsAmountA[6] += rollA
+
+			if rollB == 1:
+				rollsAmountA[rollB] += 111
+			if rollB == 2:
+				rollsAmountA[rollB] += 222
+			if rollB == 3:
+				rollsAmountA[rollB] += 333
+			if rollB == 4:
+				rollsAmountA[rollB] += 444
+			if rollB == 5:
+				rollsAmountA[rollB] += 555
+			if rollB == 6:
+				if i+1 == roundAmount:
+					rollsAmountA[rollB] += 666
+				else:
+					rollsAmountA[rollB] += 666*2
+					rollA = NormalDiceRoll()
+					rollsAmountA[int(rollA/111)] += rollA
+					i += 1
+
+			if(rollA != rollB/111):
+				if(AViciado):
+					AViciado = False
+					BViciado = True
+				else:
+					AViciado = True
+					BViciado = False
+			i+=1
+
+	return AViciado
 
 
 def main():
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
-	print(pointsInGameWithRoundsCount(2))
+	numero = input()
+	if int(numero) > 500:
+		numero = 500
+	if(pointsInGameWithRoundsCount(numero)):
+		print(1)
+	else:
+		print(2)
 
 
 if __name__ == "__main__":
